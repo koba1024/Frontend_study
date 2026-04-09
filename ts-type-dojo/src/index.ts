a9e2fc883030813483b9c15a018038c1c6540bfc
Yoshiyuki Kobayashi <yoshi.kobayashi1024@gmail.com>
2026-04-09 20:48:46 +0900

    Required, Pick , Omit再実装

--- ts-type-dojo/src/index.ts
+++ ts-type-dojo/src/index.ts
@@ -1,3 +1,10 @@
+//ここで学んだこと。
+//Generics の基礎と応用
+// mapping modifier(?や-?)
+
+
+
+// Partial自力で再実装
 type Partial1<T> = {
     [P in keyof T]?: T[P];
 }
@@ -10,7 +17,21 @@ type User = {
 
 type PartialUser = Partial1<User>;
 
-const user: PartialUser = { name: "anonymous" };
+const user1: PartialUser = { name: "anonymous" };
 
-console.log(user);
+console.log(user1);
 
+// Required自力で再実装
+type Required1<T> = {
+    [P in keyof T]-?: T[P];
+}
+
+// PartialUserをRequiredに入れたら？
+type RequiredUser = Required1<PartialUser>;
+// ageとemailも追加するように警告がでる
+// const user2: RequiredUser = { name: "anonymous" };
+
+// ageとemailも記載すると警告はでない。
+// プロパティの順番は関係ない
+const user2: RequiredUser = { age: 10, email: "xxx@xxx.com", name: "anonymous" };
+console.log(user2);
