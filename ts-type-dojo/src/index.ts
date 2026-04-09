08c88f394425096ab96c29ec737bdf4a0a686703
Yoshiyuki Kobayashi <yoshi.kobayashi1024@gmail.com>
2026-04-10 08:17:20 +0900

    Exclude, Extract再実装

--- ts-type-dojo/src/index.ts
+++ ts-type-dojo/src/index.ts
@@ -2,8 +2,6 @@
 //Generics の基礎と応用
 // mapping modifier(?や-?)
 
-
-
 // Partial自力で再実装
 type Partial1<T> = {
     [P in keyof T]?: T[P];
@@ -35,3 +33,28 @@ type RequiredUser = Required1<PartialUser>;
 // プロパティの順番は関係ない
 const user2: RequiredUser = { age: 10, email: "xxx@xxx.com", name: "anonymous" };
 console.log(user2);
+
+
+// Pick自力で再実装
+// extends: K は keyof T の範囲内でないとダメ
+type Pick1<T, K extends keyof T> = {
+    [P in K]: T[P];
+}
+
+type User2 = Pick1<User, "name" | "age">;
+
+// Omit自力で再実装
+// キーを never にすると、そのキーは mapped type の結果から消える
+type Omit1<T, K extends keyof any> = {
+    [P in keyof T as P extends K ? never : P]: T[P];
+}
+// emailのみになる
+type User3 = Omit1<User, "name" | "age">;
+
+// Exclude自力で再実装
+// TからUを取り除く Union型の操作
+type Exclude1<T, U> = T extends U ? never : T;
+
+// Extract自力で再実装
+// TからUだけを取り出す Union型の操作
+type Extract1<T, U> = T extends U ? T : never;
