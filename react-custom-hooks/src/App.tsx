0c4e2443507342795db35b04e633b9121ad15f22
Yoshiyuki Kobayashi <yoshi.kobayashi1024@gmail.com>
2026-04-16 07:47:28 +0900

    各デモをセクションごとに表示

--- react-custom-hooks/src/App.tsx
+++ react-custom-hooks/src/App.tsx
@@ -1,20 +1,42 @@
-// import UseFetchDemo from "./demos/UseFetchDemo";
-// import UseLocalStorageDemo from "./demos/useLocalStorageDemo";
-
+import UseFetchDemo from "./demos/UseFetchDemo";
+import UseLocalStorageDemo from "./demos/useLocalStorageDemo";
+import UseUndoRedoDemo from "./demos/UseUndoRedoDemo";
 import UseIntersectionObserverDemo from "./demos/UseIntersectionObserverDemo";
-
-// import UseWindowSizeDemo from "./demos/UseWindowSizeDemo";
-
-// import UseDebounceDemo from "./demos/UseDebounceDemo";
+import UseWindowSizeDemo from "./demos/UseWindowSizeDemo";
+import UseDebounceDemo from "./demos/UseDebounceDemo";
 
 function App() {
 	return (
-		<div>
-			{/* <UseFetchDemo /> */}
-			{/* <UseLocalStorageDemo /> */}
-			{/* <UseDebounceDemo /> */}
-			{/* <UseWindowSizeDemo /> */}
-			<UseIntersectionObserverDemo />
+		<div style={{ padding: "24px", display: "grid", gap: "24px" }}>
+			<section>
+				<h2>useFetch</h2>
+				<UseFetchDemo />
+			</section>
+
+			<section>
+				<h2>useLocalStorage</h2>
+				<UseLocalStorageDemo />
+			</section>
+
+			<section>
+				<h2>useDebounce</h2>
+				<UseDebounceDemo />
+			</section>
+
+			<section>
+				<h2>useWindowSize</h2>
+				<UseWindowSizeDemo />
+			</section>
+
+			<section>
+				<h2>useIntersectionObserver</h2>
+				<UseIntersectionObserverDemo />
+			</section>
+
+			<section>
+				<h2>useUndoRedo</h2>
+				<UseUndoRedoDemo />
+			</section>
 		</div>
 	);
 }
